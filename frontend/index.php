5b855d337d5d2a4f8a2157062ec8625a98b7f1c8
Philipp Moers <soziflip@gmail.com>
2021-09-29 08:35:57 +0200

    Disable caching

--- frontend/index.php
+++ frontend/index.php
@@ -41,6 +41,10 @@ if (!empty($_POST['action'])) {
     }
 }
 
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+
 ?>
 
 <!DOCTYPE html>
